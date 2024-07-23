c3b7db29baffd51eca57960bebd7444f6fc00f42
EricLima1993 <merick_lima@hotmail.com>
2024-07-22 22:06:36 -0300

    Desenvolvimento da função de editar um cliente

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -14,8 +14,16 @@ export class ClientesService {
     return this.http.post("http://localhost:8080/api/clientes/criar", cliente);
   }
 
+  atualizar(cliente:Cliente):Observable<any>{
+    return this.http.put(`http://localhost:8080/api/clientes/atualizar/${cliente.id}`, cliente);
+  }
+
   getClientes():Observable<Cliente[]>{
 
     return this.http.get<Cliente[]>("http://localhost:8080/api/clientes/buscar");
+  }
+
+  getClienteById(id:number):Observable<Cliente>{
+    return this.http.get<Cliente>(`http://localhost:8080/api/clientes/buscar/${id}`);
   }
 }
--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClientesService } from '../../clientes.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-clientes-form',
@@ -13,17 +13,28 @@ export class ClientesFormComponent implements OnInit {
   cliente!: Cliente;
   success:boolean = false;
   errors!:string[];
+  id!: number;
 
-  constructor(private service:ClientesService, private router:Router){
+  constructor(private service:ClientesService, private router:Router, private activatedRoute:ActivatedRoute){
     this.cliente = new Cliente();
   }
 
   onSubmit(){
-    this.service.salvar(this.cliente).subscribe(response => {this.success = true; this.errors = []; this.cliente = response},errorResponse => {this.errors = errorResponse.error.errors; this.success = false});
+    if(this.id){
+      this.service.atualizar(this.cliente).subscribe(response => {this.success = true; this.errors = [];}, errorResponse => {this.errors = errorResponse.error.errors; this.success = false});
+    }else{
+      this.service.salvar(this.cliente).subscribe(response => {this.success = true; this.errors = []; this.cliente = response}, errorResponse => {this.errors = errorResponse.error.errors; this.success = false});
+    }
   }
 
   ngOnInit(): void {
-    
+    let params = this.activatedRoute.params;
+    params.subscribe(urlParams => {
+      this.id = urlParams['id'];
+      if(this.id){
+        this.service.getClienteById(this.id).subscribe(response => {this.cliente = response}, errorResponse =>{});
+      }
+    });
   }
 
   voltarParaListagem(){
