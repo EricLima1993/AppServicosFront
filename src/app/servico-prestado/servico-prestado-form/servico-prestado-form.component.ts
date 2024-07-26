43c9c34bb861d87daae641ffc68d3d860e7058a3
EricLima1993 <merick_lima@hotmail.com>
2024-07-26 20:21:23 -0300

    Cricao do modulo de servico prestado e dos componentes de formulario e listagem

---
+++ src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../../clientes/clientes-form/cliente';
+import { ClientesService } from '../../clientes.service';
+import { ServicoPrestado } from '../servicoPrestado';
+
+@Component({
+  selector: 'app-servico-prestado-form',
+  templateUrl: './servico-prestado-form.component.html',
+  styleUrl: './servico-prestado-form.component.scss'
+})
+export class ServicoPrestadoFormComponent implements OnInit{
+
+  clientes: Cliente[] = [];
+  servico:ServicoPrestado;
+
+  constructor(private clienteSevice: ClientesService){
+    this.servico = new ServicoPrestado();
+  }
+  ngOnInit(): void {
+    this.clienteSevice.getClientes().subscribe(response => {this.clientes = response;});
+  }
+
+  onSubmit(){
+    
+  }
+
+}
