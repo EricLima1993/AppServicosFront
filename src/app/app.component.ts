1cd0e154c105c8197ec54e960829da29119e22fb
EricLima1993 <merick_lima@hotmail.com>
2026-09-18 17:44:28 -0300

    Ajustes de layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,11 @@
-import { AfterViewInit, Component,  } from '@angular/core';
-import jQuery from 'jquery';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
   title = 'vendas-app';
 
-  ngAfterViewInit(): void {
-    (function($) {
-      "use strict";
-  
-      var path = window.location.href;
-      
-      $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
-        if (this instanceof HTMLAnchorElement && this.href === path) {
-          $(this).addClass("active");
-        }
-      });
-      $("#sidebarToggle").on("click", function(e) {
-          e.preventDefault();
-          $("body").toggleClass("sb-sidenav-toggled");
-      });
-    })(jQuery);
-  }
 }
---
+++ src/app/layout/layout.component.ts
@@ -0,0 +1,28 @@
+import { AfterViewInit, Component } from '@angular/core';
+import jQuery from 'jquery';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss'
+})
+export class LayoutComponent implements AfterViewInit  {
+
+  ngAfterViewInit(): void {
+    (function($) {
+      "use strict";
+  
+      var path = window.location.href;
+      
+      $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
+        if (this instanceof HTMLAnchorElement && this.href === path) {
+          $(this).addClass("active");
+        }
+      });
+      $("#sidebarToggle").on("click", function(e) {
+          e.preventDefault();
+          $("body").toggleClass("sb-sidenav-toggled");
+      });
+    })(jQuery);
+  }
+}
